59c4e366a492a406d53b91b7b048cbbdc18ec61c
liviabandeira <livia.bandeira@ffm.com.br>
2018-04-06 11:32:39 -0300

    primeiro commit

---
+++ src/main/java/com/eventosapp/DataConfiguration.java
@@ -0,0 +1,38 @@
+package com.eventosapp;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		dataSource.setUsername("lbb");
+		dataSource.setPassword("sweetly");
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.ORACLE);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.Oracle9Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+
+}
---
+++ src/main/java/com/eventosapp/models/Evento.java
@@ -0,0 +1,72 @@
+package com.eventosapp.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import net.bytebuddy.implementation.bind.annotation.Empty;
+
+@Entity
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long codigo;
+	
+	@NotEmpty
+	private String nome; 
+	
+	@NotEmpty
+	private String local;
+	
+	@NotEmpty
+	private String data; 
+	
+	@NotEmpty
+	private String horario;
+	
+	@OneToMany
+	private List<Convidado> convidados;
+	
+	public String getNome() {
+		return nome;
+	}
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+}
